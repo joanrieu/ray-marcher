6fa1d67c56ee86ebbff11718ab99e4db11578d9b
Joan Rieu <joan@joanrieu.com>
2019-06-30 15:28:29 +0200

    draft of renderer structure

--- path-tracer.ts
+++ path-tracer.ts
@@ -49,7 +49,6 @@ namespace PathTracer {
 
   type Intersection = {
     point: Vec3;
-    normal: Vec3;
     mesh: Mesh;
   };
 
@@ -84,4 +83,56 @@ namespace PathTracer {
   const screen: Screen = {
     size: [30, 20]
   };
+
+  render();
+
+  function render() {
+    const canvas = document.createElement("canvas");
+    canvas.width = screen.size[0];
+    canvas.height = screen.size[1];
+    document.body.appendChild(canvas);
+    const ctx = canvas.getContext("2d");
+    const image = ctx.createImageData(canvas.width, canvas.height);
+
+    for (let y = 0; y < canvas.height; ++y) {
+      for (let x = 0; x < canvas.width; ++x) {
+        const pixel: Vec2 = [x, y];
+
+        const ray: Ray = {
+          origin: camera.position,
+          direction: camera.lookAt
+        };
+
+        const mesh = scene.meshes[0];
+        if (mesh.geometry.type !== "sphere") throw "TODO";
+
+        const point: Vec3 = [
+          mesh.geometry.center[0],
+          mesh.geometry.center[1],
+          mesh.geometry.center[2] + mesh.geometry.radius
+        ];
+
+        const intersection: Intersection = {
+          mesh,
+          point
+        };
+
+        const path: Path = {
+          ray,
+          intersection
+        };
+
+        const color = path.intersection
+          ? [...path.intersection.mesh.material.color, 255]
+          : [255, 0, 255, 255];
+
+        image.data[y * 4 * canvas.width + x * 4 + 0] = color[0];
+        image.data[y * 4 * canvas.width + x * 4 + 1] = color[1];
+        image.data[y * 4 * canvas.width + x * 4 + 2] = color[2];
+        image.data[y * 4 * canvas.width + x * 4 + 3] = color[3];
+      }
+    }
+
+    ctx.putImageData(image, 0, 0);
+  }
 }
