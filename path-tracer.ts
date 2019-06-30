3ff54ea6f49340625ea6f342bde4c0126499c09e
Joan Rieu <joan@joanrieu.com>
2019-06-30 17:14:58 +0200

    fix bugs and add plane intersection

--- path-tracer.ts
+++ path-tracer.ts
@@ -52,7 +52,7 @@ namespace PathTracer {
 
   type Intersection = {
     distance: number;
-    point: Vec3;
+    isClose: boolean;
     mesh: Mesh;
   };
 
@@ -78,7 +78,7 @@ namespace PathTracer {
   };
 
   const camera: Camera = {
-    position: [0, 0, -5],
+    position: [0, 0, 5],
     lookAt: [0, 0, 0],
     up: [0, 1, 0],
     fov: 90,
@@ -96,13 +96,13 @@ namespace PathTracer {
     canvas.width = screen.size[0];
     canvas.height = screen.size[1];
     document.body.appendChild(canvas);
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d")!;
     const image = ctx.createImageData(canvas.width, canvas.height);
 
     for (let y = 0; y < canvas.height; ++y) {
       for (let x = 0; x < canvas.width; ++x) {
-        const ray: Ray = pixel2ray([x, y]);
-        const color = trace_ray(ray);
+        const ray = pixel2ray([x, y], camera);
+        const color = trace_ray(ray, scene);
 
         image.data[y * 4 * canvas.width + x * 4 + 0] = color[0];
         image.data[y * 4 * canvas.width + x * 4 + 1] = color[1];
@@ -114,44 +114,48 @@ namespace PathTracer {
     ctx.putImageData(image, 0, 0);
   }
 
-  function trace_ray(ray: Ray) {
-    const intersection: Intersection = find_intersection(ray, scene);
-    const path: Path = { ray, intersection };
-    const color = path2color(path);
+  function trace_ray(ray: Ray, scene: Scene) {
+    const intersection = find_intersection(ray, scene);
+    const path = { ray, intersection };
+    const color = path2color(path, scene);
     return color;
   }
 
-  function find_intersection(ray: Ray, scene: Scene): Intersection {
+  function find_intersection(ray: Ray, scene: Scene) {
     let point = ray.origin;
-    let intersection: Intersection;
     let steps = 0;
+    let intersection: Intersection;
     do {
-      ++steps;
       intersection = scene.meshes
         .map(mesh => find_intersection_once(point, mesh))
         .reduce((a, b) => (a.distance < b.distance ? a : b));
-    } while (intersection.distance > 0.1 && steps < 10);
-    return intersection;
+      point = add(point, scale(intersection.distance, ray.direction));
+    } while (!intersection.isClose && ++steps < 100);
+    if (intersection.isClose) return intersection;
   }
 
-  function path2color(path: Path) {
+  function path2color(path: Path, scene: Scene) {
     return path.intersection
       ? [...path.intersection.mesh.material.color, 255]
       : scene.ambientColor;
   }
 
   function find_intersection_once(point: Vec3, mesh: Mesh): Intersection {
+    const distance = mesh_distance(point, mesh);
+    const isClose = distance < 0.1;
     return {
-      distance: mesh_distance(point, mesh),
-      point,
+      distance,
+      isClose,
       mesh
     };
   }
 
-  function mesh_distance(point: Vec3, mesh: Mesh<Geometry>) {
+  function mesh_distance(point: Vec3, mesh: Mesh) {
     switch (mesh.geometry.type) {
       case "sphere":
         return sphere_distance(point, mesh.geometry);
+      case "plane":
+        return plane_distance(point, mesh.geometry);
     }
   }
 
@@ -159,12 +163,25 @@ namespace PathTracer {
     return norm(subtract(sphere.center, point)) - sphere.radius;
   }
 
+  function plane_distance(point: Vec3, plane: Plane) {
+    const diagonal = subtract(point, plane.point);
+    return dot_product(diagonal, plane.normal);
+  }
+
+  function dot_product([a, b, c]: Vec3, [d, e, f]: Vec3) {
+    return a * d + b * e + c * f;
+  }
+
+  function add([a, b, c]: Vec3, [d, e, f]: Vec3): Vec3 {
+    return [a + d, b + e, c + f];
+  }
+
   function subtract([a, b, c]: Vec3, [d, e, f]: Vec3): Vec3 {
     return [a - d, b - e, c - f];
   }
 
-  function normSq([a, b, c]: Vec3) {
-    return a * a + b * b + c * c;
+  function normSq(v: Vec3) {
+    return dot_product(v, v);
   }
 
   function norm(v: Vec3) {
@@ -179,10 +196,15 @@ namespace PathTracer {
     return [s * a, s * b, s * c];
   }
 
-  function pixel2ray(pixel: Vec2): Ray {
+  function pixel2ray(pixel: Vec2, camera: Camera): Ray {
+    const origin: Vec3 = [
+      camera.position[0] + pixel[0] / 10,
+      camera.position[1] - pixel[1] / 10,
+      camera.position[2]
+    ];
     return {
-      origin: camera.position,
-      direction: normalize(camera.lookAt)
+      origin,
+      direction: normalize(subtract(camera.lookAt, camera.position))
     };
   }
 }
