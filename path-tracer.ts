4a27ff0f2bed0438535385bf99399ab3b7152ea2
Joan Rieu <joan@joanrieu.com>
2019-06-30 16:18:28 +0200

    implement path tracer for spheres

--- path-tracer.ts
+++ path-tracer.ts
@@ -1,19 +1,22 @@
 namespace PathTracer {
   type Scene = {
     meshes: Mesh[];
+    ambientColor: Color;
   };
 
-  type Geometry =
-    | {
-        type: "sphere";
-        center: Vec3;
-        radius: number;
-      }
-    | {
-        type: "plane";
-        point: Vec3;
-        normal: Vec3;
-      };
+  type Geometry = Sphere | Plane;
+
+  type Sphere = {
+    type: "sphere";
+    center: Vec3;
+    radius: number;
+  };
+
+  type Plane = {
+    type: "plane";
+    point: Vec3;
+    normal: Vec3;
+  };
 
   type Vec3 = [number, number, number];
 
@@ -48,13 +51,14 @@ namespace PathTracer {
   };
 
   type Intersection = {
+    distance: number;
     point: Vec3;
     mesh: Mesh;
   };
 
-  type Mesh = {
+  type Mesh<T extends Geometry = Geometry> = {
     material: Material;
-    geometry: Geometry;
+    geometry: T;
   };
 
   const scene: Scene = {
@@ -69,7 +73,8 @@ namespace PathTracer {
           color: [255, 0, 0]
         }
       }
-    ]
+    ],
+    ambientColor: [255, 0, 255]
   };
 
   const camera: Camera = {
@@ -96,35 +101,8 @@ namespace PathTracer {
 
     for (let y = 0; y < canvas.height; ++y) {
       for (let x = 0; x < canvas.width; ++x) {
-        const pixel: Vec2 = [x, y];
-
-        const ray: Ray = {
-          origin: camera.position,
-          direction: camera.lookAt
-        };
-
-        const mesh = scene.meshes[0];
-        if (mesh.geometry.type !== "sphere") throw "TODO";
-
-        const point: Vec3 = [
-          mesh.geometry.center[0],
-          mesh.geometry.center[1],
-          mesh.geometry.center[2] + mesh.geometry.radius
-        ];
-
-        const intersection: Intersection = {
-          mesh,
-          point
-        };
-
-        const path: Path = {
-          ray,
-          intersection
-        };
-
-        const color = path.intersection
-          ? [...path.intersection.mesh.material.color, 255]
-          : [255, 0, 255, 255];
+        const ray: Ray = pixel2ray([x, y]);
+        const color = trace_ray(ray);
 
         image.data[y * 4 * canvas.width + x * 4 + 0] = color[0];
         image.data[y * 4 * canvas.width + x * 4 + 1] = color[1];
@@ -134,5 +112,77 @@ namespace PathTracer {
     }
 
     ctx.putImageData(image, 0, 0);
+  }
+
+  function trace_ray(ray: Ray) {
+    const intersection: Intersection = find_intersection(ray, scene);
+    const path: Path = { ray, intersection };
+    const color = path2color(path);
+    return color;
+  }
+
+  function find_intersection(ray: Ray, scene: Scene): Intersection {
+    let point = ray.origin;
+    let intersection: Intersection;
+    let steps = 0;
+    do {
+      ++steps;
+      intersection = scene.meshes
+        .map(mesh => find_intersection_once(point, mesh))
+        .reduce((a, b) => (a.distance < b.distance ? a : b));
+    } while (intersection.distance > 0.1 && steps < 10);
+    return intersection;
+  }
+
+  function path2color(path: Path) {
+    return path.intersection
+      ? [...path.intersection.mesh.material.color, 255]
+      : scene.ambientColor;
+  }
+
+  function find_intersection_once(point: Vec3, mesh: Mesh): Intersection {
+    return {
+      distance: mesh_distance(point, mesh),
+      point,
+      mesh
+    };
+  }
+
+  function mesh_distance(point: Vec3, mesh: Mesh<Geometry>) {
+    switch (mesh.geometry.type) {
+      case "sphere":
+        return sphere_distance(point, mesh.geometry);
+    }
+  }
+
+  function sphere_distance(point: Vec3, sphere: Sphere) {
+    return norm(subtract(sphere.center, point)) - sphere.radius;
+  }
+
+  function subtract([a, b, c]: Vec3, [d, e, f]: Vec3): Vec3 {
+    return [a - d, b - e, c - f];
+  }
+
+  function normSq([a, b, c]: Vec3) {
+    return a * a + b * b + c * c;
+  }
+
+  function norm(v: Vec3) {
+    return Math.sqrt(normSq(v));
+  }
+
+  function normalize(v: Vec3): Vec3 {
+    return scale(1 / norm(v), v);
+  }
+
+  function scale(s: number, [a, b, c]: Vec3): Vec3 {
+    return [s * a, s * b, s * c];
+  }
+
+  function pixel2ray(pixel: Vec2): Ray {
+    return {
+      origin: camera.position,
+      direction: normalize(camera.lookAt)
+    };
   }
 }
